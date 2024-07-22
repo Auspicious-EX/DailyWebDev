e3cb2b37f39e3780bb7767ca04655dc3049a250e
Auspicious-EX <shubhamstudy19@gmail.com>
2024-07-22 22:23:39 +0530

    T48 Completed

---
+++ Day 48/script.js
@@ -0,0 +1,52 @@
+console.log("Hello Shubham")
+
+let a = 5;
+a = a + 1
+var s = 3
+let b = 4;
+let c = a + b;
+let d = "Shubham"
+// var 3a = "Hello Shubham" invalid
+
+console.log (a)
+console.log (d)
+console.log (typeof a, typeof b, typeof c , typeof d )
+
+
+{
+    let a = 34
+    console.log (a)
+
+    var s = 45
+    console.log (s)
+}
+console.log (a) 
+console.log (s)
+
+// const a1 = 2
+// a1 = a1 + 1
+
+
+
+let x = "Shubham ok";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x,typeof  y, typeof z, typeof p,typeof  q, typeof r)
+
+
+let o = {
+    "name" : "Shubham",
+    "Job is" : "CEO",
+    "is_good" : true
+}
+
+console.log(o)
+o.salaty = " 1 lakh"
+console.log(o)
+o.salaty = " 5 lakh"
+console.log(o)
