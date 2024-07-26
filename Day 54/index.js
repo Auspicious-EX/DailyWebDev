0af3df5295f10e8630f67c450c0a56ba787d965e
Auspicious-EX <shubhamstudy19@gmail.com>
2024-07-26 21:48:59 +0530

    T54 Completed

---
+++ Day 54/index.js
@@ -0,0 +1,20 @@
+/* Create a Bsuiness name generator by combining list of adjecctives and shop name and another word
+
+Adjectives:
+crazy 
+amezing
+fire
+
+Shop Name:
+Engine
+foods
+Garments
+
+Another Word:
+
+Bros
+limited
+Hub */
+
+
+
