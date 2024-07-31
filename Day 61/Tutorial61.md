5f5370257aca75e01cf5ba48ef988d01f84db425
Auspicious-EX <shubhamstudy19@gmail.com>
2024-07-31 10:38:10 +0530

    T61 E13 Completed

--- Day 61/Tutorial61.md
+++ Day 61/Tutorial61.md
@@ -1,1 +1,7 @@
 # Tutorial **61** Notes
+
+## Exercise 13
+
+***make card using JS***
+
+#### Note : Chake solution one time so you have idea to how build this. 
---
+++ Day 61/index.js
@@ -0,0 +1,69 @@
+function createCard(titlename, cname, views, time, dur, backgroundImage) {
+    const container = document.createElement('div');
+    container.className = 'container';
+
+    const imageDiv = document.createElement('div');
+    imageDiv.className = 'image';
+    imageDiv.style.backgroundImage = `url(${backgroundImage})`;
+
+    const durSpan = document.createElement('span');
+    durSpan.className = 'dur';
+    durSpan.textContent = dur;
+    imageDiv.appendChild(durSpan);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.className = 'info';
+
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+
+    const titleSpan = document.createElement('span');
+    titleSpan.className = 'titlename';
+    titleSpan.textContent = titlename;
+    titleDiv.appendChild(titleSpan);
+
+    const discDiv = document.createElement('div');
+    discDiv.className = 'disc';
+
+    const cnameSpan = document.createElement('span');
+    cnameSpan.className = 'cname';
+    cnameSpan.textContent = cname;
+    discDiv.appendChild(cnameSpan);
+
+    const viewsSpan = document.createElement('span');
+    viewsSpan.className = 'views';
+    viewsSpan.textContent = `${views}K views`;
+    discDiv.appendChild(viewsSpan);
+
+    const timeSpan = document.createElement('span');
+    timeSpan.className = 'time';
+    timeSpan.textContent = `${time} months ago`;
+    discDiv.appendChild(timeSpan);
+
+    infoDiv.appendChild(titleDiv);
+    infoDiv.appendChild(discDiv);
+
+    container.appendChild(imageDiv);
+    container.appendChild(infoDiv);
+
+    document.getElementById('card-container').appendChild(container);
+}
+
+createCard(
+    "Introduction to Backend | Sigma Web Dev video #2",
+    "CodeWithHarry",
+    560,
+    7,
+    "31:22",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+);
+
+createCard(
+    "Introduction to Backend | Sigma Web Dev video #78",
+    "CodeWithHarry",
+    50,
+    7,
+    "31:22",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+);
+
