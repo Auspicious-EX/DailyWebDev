d39ebbdabe1dce0c8d3a5ad88fa6faa361cada4d
Auspicious-EX <shubhamstudy19@gmail.com>
2024-09-04 13:12:43 +0530

    T102 Completed

---
+++ Day 102/first/app/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> i am page
+      <h1 className="text-xl">Problem solved by next.js</h1>
+      <ul>
+        <li>Fullstack solution</li>
+        <li>File based routing</li>
+        <li>additional feature like : router from next / navigation</li>
+        <li>optimize rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
